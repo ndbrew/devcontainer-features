2af975c29a82348eb6fc1ef8b3c0ab416f268d16
ndbrew <3947833+ndbrew@users.noreply.github.com>
2025-05-08 22:12:24 +0000

    fix: Fix version bumps for ansible and pre-commit

--- src/pre-commit/install.sh
+++ src/pre-commit/install.sh
@@ -14,7 +14,7 @@ ensure_nanolayer nanolayer_location "v0.5.6"
 $nanolayer_location \
     install \
     devcontainer-feature \
-    "ghcr.io/ndbrew/devcontainer-features/pipx-package:1.1.8" \
+    "ghcr.io/ndbrew/devcontainer-features/pipx-package:1.1.10" \
     --option package='pre-commit' --option version="$VERSION"
     
 
